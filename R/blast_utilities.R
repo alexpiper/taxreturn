b9f359a76f917c510289a371b25bcdd41942b213
alexpiper <alexpiperdesigns@gmail.com>
2020-08-10 13:38:34 +1000

    Updated blast inputs

--- R/blast_utilities.R
+++ R/blast_utilities.R
@@ -155,11 +155,17 @@ blast_params <- function(type = "blastn") {
 #'
 #' @param query (Required) Query sequence. Accepts a DNABin object, DNAStringSet object, Character string, or filepath.
 #' @param db (Required) Reference sequences to conduct search against. Accepts a DNABin object, DNAStringSet object, Character string, or filepath.
-#' If DNAbin, DNAStringSet or character string is provided, a temporary fasta file is used to construct BLAST database
+#' If DNAbin, DNAStringSet or character string is provided, a temporary fasta file is used to construct BLAST database.
+#' If db is set to "remote", this will conduct a search against NCBI nucleotide database.
 #' @param type (Required) type of search to conduct, default 'blastn'
 #' @param evalue (Required) Minimum evalue from search
+#' @param output_format The output format to be returned.
+#'  Default is tabular, which returns 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qcov
+#' See https://www.ncbi.nlm.nih.gov/books/NBK279684/ for custom formatting information
 #' @param args (Optional) Extra arguments passed to BLAST
+#' @param ungapped Whether ungapped alignment should be conducted. Default is FALSE.
 #' @param quiet (Optional) Whether progress should be printed to console, default is FALSE
+#' @param multithread Whether multithreading should be used, if TRUE the number of cores will be automatically detected, or provided a numeric vector to manually set the number of cores to use
 #'
 #' @return
 #' @export
@@ -168,9 +174,14 @@ blast_params <- function(type = "blastn") {
 #' @import stringr
 #' @import tibble
 #' @import dplyr
+#' @import future
 #'
 #' @examples
-blast <- function (query, db, type="blastn", evalue = 1e-6, output_format = "tabular", args=NULL, quiet=FALSE){
+#'
+#' Add qcov length filter
+blast <- function (query, db, type="blastn", evalue = 1e-6,
+                   output_format = "tabular", args=NULL, ungapped=FALSE,
+                   quiet=FALSE, multithread=FALSE){
   time <- Sys.time() # get time
   # Create temp files
   tmp <- tempdir()
@@ -179,28 +190,59 @@ blast <- function (query, db, type="blastn", evalue = 1e-6, output_format = "tab
 
   .findExecutable(type) # check blast is installed
 
+  #Setup multithreading
+  ncores <- future::availableCores() -1
+  if((isTRUE(multithread) | is.numeric(multithread) & multithread > 1) & db=="remote"){
+    stop("Multithreading must be set to false for remote searches")
+  } else if(isTRUE(multithread)){
+    cores <- ncores
+    if(!quiet){message("Multithreading with ", cores, " cores")}
+  } else if (is.numeric(multithread) & multithread > 1){
+    cores <- multithread
+    if(cores > ncores){
+      cores <- ncores
+      message("Warning: the value provided to multithread is higher than the number of cores, using ", cores, " cores instead")
+    }
+    if(!quiet){message("Multithreading with ", cores, " cores")}
+  } else if(isFALSE(multithread) | multithread==1){
+    cores <- 1
+  } else (
+    stop("Multithread must be a logical or numeric vector of the numbers of cores to use")
+  )
+
   # Check outfmt
   if(output_format=="tabular"){
-    outfmt <- 6
-  } else if (output_format=="lulu"){
-    outfmt <- "'6 qseqid sseqid pident'"
-  } else if(!output_format==6 && is.numeric(output_format)){
+    #Custom tabular format
+    parsecols <- c("qseqid", "sseqid", "pident", "length", "mismatch", "gapopen",
+                   "qstart", "qend", "sstart", "send", "evalue", "bitscore", "qcovs", "qcovhsp")
+    outfmt <- paste0("\"",6," ", paste(parsecols, collapse=" "),"\"")
+  } else if(is.numeric(output_format)){
     outfmt <- output_format
-  } else if(!output_format %in% c("tabular", "lulu") && is.character(output_format)){
-    outfmt <- paste0("'",output_format, "'")
+    parsecols <- NULL
+  } else if (!is.na(str_extract(output_format, "."))){
+    outfmt <- paste0("\"",output_format,"\"")
+    parsecols <- output_format %>%
+      str_remove("^..") %>%
+      str_split_fixed("\ ", n=Inf) %>%
+      as.character()
   }
 
   # Database
-  if(inherits(db, "DNAbin")){
+  if(db=="remote"){
+    db <- "nt"
+    remote <- "-remote"
+  } else if(inherits(db, "DNAbin")){
     if (!quiet) { message("Database input is DNAbin: Creating temporary blast database") }
     insect::writeFASTA(db, tmpdb)
     makeblastdb(tmpdb)
     db <- tmpdb
+    remote <- ""
   } else if (inherits(db, "DNAString") | inherits(db, "DNAStringSet")){
     if (!quiet) { message("Database input is DNAStringSet: Creating temporary blast database") }
     Biostrings::writeXStringSet(db, tmpdb)
     makeblastdb(tmpdb)
     db <- tmpdb
+    remote <- ""
   } else if (inherits(db, "character") &&  all(stringr::str_to_upper(stringr::str_split(db,"")[[1]]) %in% Biostrings::DNA_ALPHABET)) { # Handle text input
     if (!quiet) { message("Database input is character string: Creating temporary blast database") }
     if (nchar(db[1]) == 1) {db <- paste0(db, collapse = "")}
@@ -208,10 +250,11 @@ blast <- function (query, db, type="blastn", evalue = 1e-6, output_format = "tab
     insect::writeFASTA(db, tmpdb)
     makeblastdb(tmpdb)
     db <- tmpdb
+    remote <- ""
   } else if (inherits(db, "character") &&  file.exists(file.path(db))){ # Handle filename
     makeblastdb(db)
     db <- stringr::str_replace(db, ".gz", "")
-    db <- db
+    remote <- ""
   } else {
     stop("Invalid BLAST database")
   }
@@ -219,16 +262,20 @@ blast <- function (query, db, type="blastn", evalue = 1e-6, output_format = "tab
   # Query
   if(inherits(query, "DNAbin")){
     if (!quiet) { message("Query is DNAbin: Creating temporary fasta file") }
+    query <- ape::del.gaps(query)
     insect::writeFASTA(query, tmpquery)
     input <- tmpquery
   } else if (inherits(query, "DNAString") | inherits(query, "DNAStringSet")){
     if (!quiet) { message("Query is DNAString: Creating temporary fasta file") }
-    Biostrings::writeXStringSet(query, tmpquery)
+    query <- ape::as.DNAbin(query)
+    query <- ape::del.gaps(query)
+    insect::writeFASTA(query, tmpquery)
     input <- tmpquery
   }else if (inherits(query, "character") &&  all(str_to_upper(str_split(query,"")[[1]]) %in% Biostrings::DNA_ALPHABET)) { # Handle text query
     if (!quiet) { message("Query is character string: Creating temporary fasta file") }
     if (nchar(query[1]) == 1) {query <- paste0(query, collapse = "")}
     query <- insect::char2dna(query)
+    query <- ape::del.gaps(query)
     insect::writeFASTA(query, tmpquery)
     input <- tmpquery
   } else if (inherits(query, "character") &&  file.exists(file.path(query))){ # Handle filenames
@@ -237,35 +284,39 @@ blast <- function (query, db, type="blastn", evalue = 1e-6, output_format = "tab
     stop("Invalid BLAST query")
   }
 
+  #Setup ungapped
+  ungapped <- ifelse(ungapped, "-ungapped ","")
+
+  nthreads <- ifelse(cores > 1, paste0("-num_threads ",unname(cores)), "")
+
   #  Conduct BLAST search
-  if (!quiet) { message("Running BLAST") }
+  if (!quiet) { message("Running BLASTn query: ", paste(c("-db", db,
+                              "-query", input,
+                              remote,
+                              "-outfmt", outfmt,
+                              "-evalue", evalue,
+                              nthreads,
+                              ungapped,
+                              args), collapse=" "))}
   results <- system2(command = .findExecutable(type),
                      args = c("-db", db,
                               "-query", input,
-                              "-outfmt ", outfmt,
+                              remote,
+                              "-outfmt", outfmt,
                               "-evalue", evalue,
-                              "-ungapped", args),
+                              nthreads,
+                              ungapped,
+                              args),
                      wait = TRUE,
                      stdout = TRUE)
 
   # Parse BLAST results
-  if(output_format=="tabular"){
+  if(!is.null(parsecols)){
     out <- results %>%
       tibble::enframe() %>%
-      tidyr::separate(col = value,
-                      into = c("qseqid", "sseqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore"),
-                      sep = "\t",
-                      convert = TRUE)
-  } else if(output_format =="lulu"){
-    out <- results %>%
-      tibble::enframe() %>%
-      tidyr::separate(col = value,
-                      into = c("qseqid", "sseqid", "pident"),
-                      sep = "\t",
-                      convert = TRUE)
+      tidyr::separate(col = value, into = parsecols,  sep = "\t", convert = TRUE)
   } else{
-    message("Warning, result parsing is currently only supported for output_format = 'tabular'
-            and output_format = 'lulu', returning raw results")
+    message("Warning, result parsing is currently only supported for output_format = 'tabular', returning raw results")
     out <- results %>%
       tibble::enframe()
   }
